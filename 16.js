c9fcece070f97820cff6bc44885b358d2e4454d4
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-07 03:22:02 +0530

    arrays

---
+++ 16.js
@@ -0,0 +1,14 @@
+//arrays
+
+//reference type
+
+let fruit = ['apple', 'mango', 'grapes']
+let num = [1, 2, 3, 5, null, undefined, ...fruit]
+fruit.push('banana')
+console.log(fruit)
+console.log(fruit.pop())
+console.log(fruit)
+fruit.unshift('pineapple')
+console.log(fruit)
+console.log(fruit.shift())
+console.log(fruit)
