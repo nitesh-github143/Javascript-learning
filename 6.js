476d91baedd4a43165f75ced782838eef1b941c6
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-06 22:33:08 +0530

    string and its method

---
+++ 6.js
@@ -0,0 +1,9 @@
+// console.log('ji')
+
+let firstName = 'Nitesh';
+console.log(firstName[firstName.length - 1]);
+console.log(firstName.length)
+
+for (let i = 0; i < 6; i++) {
+    console.log(firstName[i])
+}
---
+++ 7.js
@@ -0,0 +1,12 @@
+// console.log('hi');
+
+let firstName = '            Nitesh      chowdhary  '
+
+console.log(firstName.length)
+firstName = firstName.trim()
+console.log(firstName.toUpperCase());
+console.log(firstName.toLocaleLowerCase());
+
+// slice method
+
+console.log(firstName.slice(5, 19))
