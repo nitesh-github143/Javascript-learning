9daded9838223f7f5175b646c112c7e086013659
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-07 04:42:42 +0530

    array and for loop

---
+++ 18.js
@@ -0,0 +1,11 @@
+// for loop
+
+let fruits = ['apple', 'mango', 'grapes', 'banana']
+let newFruit = []
+for (let i = 0; i < fruits.length; i++) {
+    newFruit.push(fruits[i].toUpperCase())
+}
+console.log(newFruit)
+// fruits.forEach(){
+//     console.log(fruit)
+// }
