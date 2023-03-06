fbc0271901a638337534f1d6cca16c43c42c36e2
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-07 01:27:34 +0530

    typeof and bigint

---
+++ 9.js
@@ -0,0 +1,14 @@
+// let firstName;
+
+
+// console.log(typeof firstName)
+
+let myVar = null;
+console.log(typeof myVar)
+
+//big int 
+
+let myNum = BigInt(122378549879387598237598743857498572983475987349587);
+console.log(typeof myNum);
+
+console.log(Number.MAX_SAFE_INTEGER)
