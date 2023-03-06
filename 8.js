35fd5dfb0b6b6f49c536e2f156de567d0ee38b12
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-06 22:55:08 +0530

    Template string

---
+++ 8.js
@@ -0,0 +1,25 @@
+// type of operator
+
+let age = 22;
+let firstName = 'Nitesh';
+
+console.log(typeof age, typeof firstName);
+
+console.log(typeof (age + ''))
+
+let mystr = '24';
+
+console.log(typeof + mystr)
+
+//string concatenation
+
+let str1 = 'nitesh'
+let str2 = 'chowdhary'
+
+let fullName = str1 + ' ' + str2
+console.log(fullName)
+
+// let aboutMe = 'My name is ' + firstName + ' my age is ' + age
+let aboutMe = `My name is ${firstName} and i am  ${age} years old `
+console.log(aboutMe)
+
