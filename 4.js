37fac0bd1e5b33c914cb23a5694fa05087fe2e55
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-06 22:12:16 +0530

    let and const

---
+++ 4.js
@@ -0,0 +1,7 @@
+// variable via let
+
+let firstName = 'Nitesh';
+console.log(firstName);
+
+//var vs let --> we cannot declare a variable twice with let but we can via var so we should not use var
+
---
+++ 5.js
@@ -0,0 +1,4 @@
+// console.log('hi')
+
+const PI = 3.14;
+console.log(PI);
