57a9133b147fb1adf6fbb851c921f22d1cf61e1f
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-07 21:50:05 +0530

    computed properties

--- 21.js
+++ 21.js
@@ -3,3 +3,18 @@ const person = {
     age: 22,
     hobbies: ['guitar', 'reading']
 }
+
+//for in loop
+
+// for (let key in person) {
+//     console.log(key, ':', person[key])
+// }
+
+//object keys
+
+console.log(Object.keys(person))
+
+for (let key of Object.keys(person)) {
+    console.log(person[key])
+}
+
---
+++ 22.js
@@ -0,0 +1,16 @@
+//computed properties
+
+const key1 = 'object1'
+const key2 = 'object2'
+
+const value1 = 'myvalue1'
+const value2 = 'myvalue2'
+
+const obj = {
+
+}
+
+obj[key1] = value1
+obj[key2] = value2
+
+console.log(obj)
