5239b8e55b42dc6a7b8a99c0d1fcd018b56c88b3
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-07 07:10:46 +0530

    array destructure

--- 18.js
+++ 18.js
@@ -2,10 +2,19 @@
 
 let fruits = ['apple', 'mango', 'grapes', 'banana']
 let newFruit = []
-for (let i = 0; i < fruits.length; i++) {
-    newFruit.push(fruits[i].toUpperCase())
+// for (let i = 0; i < fruits.length; i++) {
+//     newFruit.push(fruits[i].toUpperCase())
+// }
+
+for (let fruit of fruits) {
+    newFruit.push(fruit.toUpperCase())
 }
 console.log(newFruit)
 // fruits.forEach(){
 //     console.log(fruit)
 // }
+
+// const names = "nitesh"
+// console.log(names[3]);
+// console.log(names)
+
---
+++ 19.js
@@ -0,0 +1,8 @@
+// array destructuring
+
+const myArr = ['value1', 'value2', 'val']
+
+let [a1, , a2] = myArr
+// a1 = 'rahul'
+console.log(a1, a2)
+console.log(myArr)
